8f2e8eaaade0cb443fd1ad54fa274086266061b2
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2014-06-13 14:46:55 +0200

    Adding model classes for StandupUser and TodoItem

---
+++ Standup-iOS/Standup-iOS/Models/FDStandupUser.h
@@ -0,0 +1,13 @@
+//
+//  FDStandupuser.h
+//  Standup-iOS
+//
+//  Created by Sergii Nezdolii on 2014-06-13.
+//  Copyright (c) 2014 FrostDigital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FDStandupUser : NSObject
+
+@end
